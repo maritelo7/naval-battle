1294b7475ea6f8acae62ecc36a3de9e1a2abf9bf
javr07 <ppjavr@gmail.com>
2017-09-25 01:58:49 -0500

    Branch Incluyendo idiomas iniciarsesion

--- Naval-Battle/src/navalBattle/gui/GUI_IniciarSesionController.java
+++ Naval-Battle/src/navalBattle/gui/GUI_IniciarSesionController.java
@@ -5,9 +5,16 @@
  */
 package navalBattle.gui;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -15,13 +22,40 @@ import javafx.fxml.Initializable;
  * @author javr
  */
 public class GUI_IniciarSesionController implements Initializable {
-
+   @FXML
+   private JFXButton buttonIdioma;
+   @FXML
+   private JFXButton buttonReglas;
+   @FXML
+   private JFXButton buttonPuntuacion;
+   @FXML
+   private JFXButton buttonIniciar;
+   @FXML
+   private JFXButton buttonRegistrar;
+   @FXML
+   private JFXTextField tFieldNick;
+   @FXML
+   private JFXPasswordField pFieldClave;
+   @FXML
+   private Label labelNick;
+   @FXML
+   private Label labelClave;
+   
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+   public void initialize(URL url, ResourceBundle rb) {
+      cargarIdioma();
+   }    
+   public void cargarIdioma(){
+      Locale locale = Locale.getDefault();
+      ResourceBundle resources = ResourceBundle.getBundle("navalBattle.recursos.Idioma",locale);
+      buttonIdioma.setText(resources.getString("buttonIdioma"));
+      buttonReglas.setText(resources.getString("buttonReglas"));
+      buttonPuntuacion.setText(resources.getString("buttonPuntuacion"));
+      buttonIniciar.setText(resources.getString("buttonIniciar"));
+      buttonRegistrar.setText(resources.getString("buttonRegistrar"));
+      labelClave.setText(resources.getString("labelClave"));
+   } 
 }
