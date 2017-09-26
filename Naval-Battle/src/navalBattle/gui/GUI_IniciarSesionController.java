1d161b06dc55d46b1076a7bc673aaa041654473e
javr07 <ppjavr@gmail.com>
2017-09-25 21:47:00 -0500

    Idiomas y recursos

--- Naval-Battle/src/navalBattle/gui/GUI_IniciarSesionController.java
+++ Naval-Battle/src/navalBattle/gui/GUI_IniciarSesionController.java
@@ -10,10 +10,21 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 /**
@@ -40,22 +51,75 @@ public class GUI_IniciarSesionController implements Initializable {
    private Label labelNick;
    @FXML
    private Label labelClave;
+   public String idioma;
    
     /**
      * Initializes the controller class.
      */
     @Override
    public void initialize(URL url, ResourceBundle rb) {
-      cargarIdioma();
-   }    
-   public void cargarIdioma(){
       Locale locale = Locale.getDefault();
-      ResourceBundle resources = ResourceBundle.getBundle("navalBattle.recursos.Idioma",locale);
+      cargarIdioma(locale);
+      
+      buttonIdioma.setOnAction((ActionEvent event) -> {
+         Locale localeSelect = Locale.getDefault();
+         idioma = cargarAviso();
+         System.out.println("Idioma"+ idioma);
+         switch (idioma){
+            case "English":
+               Locale localeEng = new Locale("en", "US");
+               cargarIdioma(localeEng);
+            break;
+            case "Français":
+               Locale localeFran = new Locale("fr", "FR");
+               cargarIdioma(localeFran);
+            break;
+            default:
+            cargarIdioma(localeSelect);
+         }
+      });
+   }
+   
+   public void cargarIdioma(Locale localeSelect){
+      
+      ResourceBundle resources = ResourceBundle.getBundle("navalBattle.recursos.Idioma", localeSelect);
       buttonIdioma.setText(resources.getString("buttonIdioma"));
       buttonReglas.setText(resources.getString("buttonReglas"));
       buttonPuntuacion.setText(resources.getString("buttonPuntuacion"));
       buttonIniciar.setText(resources.getString("buttonIniciar"));
       buttonRegistrar.setText(resources.getString("buttonRegistrar"));
       labelClave.setText(resources.getString("labelClave"));
-   } 
+   }
+   public void cargarAviso(String title, String mensaje) {
+      Alert confirmacion = new Alert(Alert.AlertType.INFORMATION);
+      confirmacion.setTitle(title);
+      confirmacion.setHeaderText(null);
+      confirmacion.setContentText(mensaje);
+      ButtonType btAceptar = new ButtonType("Aceptar", ButtonBar.ButtonData.CANCEL_CLOSE);
+      confirmacion.getButtonTypes().setAll(btAceptar);
+      Optional<ButtonType> eleccion = confirmacion.showAndWait();
+   }
+   public String cargarAviso() {
+      Locale locale = Locale.getDefault();
+      ResourceBundle resources = ResourceBundle.getBundle("navalBattle.recursos.Idioma",locale);
+      String idiomaSelect;
+      Alert confirmacion = new Alert(Alert.AlertType.INFORMATION);
+      ComboBox comboIdiomas = new ComboBox();
+      List<String> listIdiomas = new ArrayList<>();
+      ObservableList<String> idiomas = FXCollections.observableList(listIdiomas);
+      idiomas.addAll("Español","Français","English");
+      comboIdiomas.setItems(idiomas);
+      confirmacion.setTitle(resources.getString("buttonIdioma"));
+      confirmacion.setHeaderText(null);
+      confirmacion.setContentText(resources.getString("textCambIdioma"));
+      confirmacion.setGraphic(comboIdiomas);
+      ButtonType btCancelar = new ButtonType(resources.getString("buttonCancelar"), ButtonBar.ButtonData.CANCEL_CLOSE);
+      ButtonType btAceptar = new ButtonType(resources.getString("buttonGuardar"));
+      confirmacion.getButtonTypes().setAll(btCancelar, btAceptar);
+      Optional<ButtonType> eleccion = confirmacion.showAndWait();
+      if (eleccion.get() == btAceptar) {
+         return comboIdiomas.getValue().toString();
+      }
+      return " ";
+   }
 }
