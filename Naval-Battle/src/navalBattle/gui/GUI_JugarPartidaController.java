00c9e377591ac58b0c753cad7f64c8f1f4945d1f
maritelo7 <zS15011664@estudiantes.uv.mx>
2017-09-25 23:43:04 -0500

    Avance de internacionalización en las GUI de Jugar Partida y Preparar partida

--- Naval-Battle/src/navalBattle/gui/GUI_JugarPartidaController.java
+++ Naval-Battle/src/navalBattle/gui/GUI_JugarPartidaController.java
@@ -6,8 +6,13 @@
 package navalBattle.gui;
 
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 
 /**
  * FXML Controller class
@@ -16,12 +21,34 @@ import javafx.fxml.Initializable;
  */
 public class GUI_JugarPartidaController implements Initializable {
 
+  @FXML
+  private Font x1;
+  @FXML
+  private Label labelColocaNaves;
+  @FXML
+  private Label labelColocaNaves2;
+  @FXML
+  private Label labelRotar;
+  @FXML
+  private Button buttonContinuar;
+  
+
   /**
    * Initializes the controller class.
    */
   @Override
   public void initialize(URL url, ResourceBundle rb) {
     // TODO
+   cargarIdioma();
   }  
+   
+  public void cargarIdioma(){
+      Locale locale = Locale.getDefault();
+      ResourceBundle resources = ResourceBundle.getBundle("navalBattle.recursos.Idioma",locale);
+      labelColocaNaves.setText(resources.getString("labelColocaNaves"));
+      labelColocaNaves2.setText(resources.getString("labelColocaNaves2"));
+      labelRotar.setText(resources.getString("labelRotar"));
+      buttonContinuar.setText(resources.getString("buttonContinuar"));
+   }
   
 }
--- Naval-Battle/src/navalBattle/gui/GUI_PrepararPartidaController.java
+++ Naval-Battle/src/navalBattle/gui/GUI_PrepararPartidaController.java
@@ -6,8 +6,13 @@
 package navalBattle.gui;
 
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 
 /**
  * FXML Controller class
@@ -16,6 +21,20 @@ import javafx.fxml.Initializable;
  */
 public class GUI_PrepararPartidaController implements Initializable {
 
+  @FXML
+  private Font x1;
+  @FXML
+  private Label labelCronometro;
+  @FXML
+  private Label labelTiempoRestante;
+  @FXML
+  private Label labelPuntuacionHost;
+  @FXML
+  private Label labelPuntuacionAdversario;
+  @FXML
+  private Button buttonRendirse;
+  
+  
   /**
    * Initializes the controller class.
    */
@@ -24,4 +43,13 @@ public class GUI_PrepararPartidaController implements Initializable {
     // TODO
   }  
   
+  public void cargarIdioma(){
+      Locale locale = Locale.getDefault();
+      ResourceBundle resources = ResourceBundle.getBundle("navalBattle.recursos.Idioma",locale);
+      labelCronometro.setText(resources.getString("labelCronometro"));
+      labelTiempoRestante.setText(resources.getString("labelTiempoRestante"));
+      labelPuntuacionHost.setText(resources.getString("labelPuntuacionHost"));
+      labelPuntuacionHost.setText(resources.getString("labelPuntuacionAdversario"));
+      buttonRendirse.setText(resources.getString("buttonRendirse"));
+   }
 }
